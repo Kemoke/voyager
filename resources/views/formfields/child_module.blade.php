d5325baf78c422d46e6ca2fd262813d7afb30aaf
vblinden <me@vblinden.com>
2018-02-13 12:12:36 +0100

    Added voyager datatables

--- resources/views/formfields/child_module.blade.php
+++ resources/views/formfields/child_module.blade.php
@@ -1,13 +1,6 @@
-{{--
-    TODO:
-        - Fix add.
-        - Fix adding relationsíhip.
---}}
-
 @php
     // Instantiate a new model which we can use in the rest of the component...
     $model = app()->make($options->model); 
-
     // Retrieve the datatype of the relationship and preset the id of parent...
     $relationshipDataType = \TCG\Voyager\Models\DataType::where('model_name', $options->model)->first();
     $relationshipDataTypeContent = $model;
@@ -22,29 +15,56 @@
     <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target=".form-modal-{{ $relationshipDataType->name }}">Add a new {{ str_singular($relationshipDataType->name) }}</button> <br/>
 </p>
     
-{{-- 
-    The table below should be replaced with the tables that Voyager uses, the implementation below is only temporary.
---}}
-<table class="table">
-    <thead>
-        <tr>
-            @foreach(collect($model->first()->toArray())->except(['id', $options->column])->toArray() as $key => $value)
-            <th>{{ ucfirst($key) }}</th>
-            @endforeach
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-            @foreach ($model->where($options->column, $dataTypeContent->id)->get() as $item)
+
+ <div class="table-responsive">
+    <table id="dataTable" class="table table-hover">
+        <thead>
             <tr>
-                @foreach(collect($item->toArray())->except(['id', $options->column])->toArray() as $value)
-                <td>{{ $value }}</td>
+                @foreach($relationshipDataType->browseRows as $row)
+                <th>{{ $row->display_name }}</th>
                 @endforeach
-                <td><a href="#">Edit</a> or <a href="#">Delete</a></td>
+                <th class="actions">{{ __('voyager.generic.actions') }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($model->where($options->column, $dataTypeContent->id)->get() as $data)
+            <tr>
+                @foreach($relationshipDataType->browseRows as $row)
+                <?php $rowOptions = json_decode($row->details); ?>
+                <td>
+                    @if($row->type == 'image')
+                        <img src="@if( !filter_var($data->{$row->field}, FILTER_VALIDATE_URL)){{ Voyager::image( $data->{$row->field} ) }}@else{{ $data->{$row->field} }}@endif" style="width:100px">
+                    @elseif($row->type == 'text')
+                        @include('voyager::multilingual.input-hidden-bread-browse')
+                        <div class="readmore">{{ mb_strlen( $data->{$row->field} ) > 200 ? mb_substr($data->{$row->field}, 0, 200) . ' ...' : $data->{$row->field} }}</div>
+                    @else
+                        @include('voyager::multilingual.input-hidden-bread-browse')
+                        <span>{{ $data->{$row->field} }}</span>
+                    @endif
+                </td>
+                @endforeach
+                <td class="no-sort no-click" id="bread-actions">
+                    @can('delete', $data)
+                        <a href="javascript:;" title="{{ __('voyager.generic.delete') }}" class="btn btn-sm btn-danger pull-right delete" data-id="{{ $data->{$data->getKeyName()} }}" id="delete-{{ $data->{$data->getKeyName()} }}">
+                            <i class="voyager-trash"></i> <span class="hidden-xs hidden-sm">{{ __('voyager.generic.delete') }}</span>
+                        </a>
+                    @endcan
+                    @can('edit', $data)
+                        <a href="{{ route('voyager.'.$relationshipDataType->slug.'.edit', $data->{$data->getKeyName()}) }}" title="{{ __('voyager.generic.edit') }}" class="btn btn-sm btn-primary pull-right edit">
+                            <i class="voyager-edit"></i> <span class="hidden-xs hidden-sm">{{ __('voyager.generic.edit') }}</span>
+                        </a>
+                    @endcan
+                    @can('read', $data)
+                        <a href="{{ route('voyager.'.$relationshipDataType->slug.'.show', $data->{$data->getKeyName()}) }}" title="{{ __('voyager.generic.view') }}" class="btn btn-sm btn-warning pull-right">
+                            <i class="voyager-eye"></i> <span class="hidden-xs hidden-sm">{{ __('voyager.generic.view') }}</span>
+                        </a>
+                    @endcan
+                </td>
             </tr>
             @endforeach
-    </tbody>
-</table>
+        </tbody>
+    </table>
+ </div>
 
 <hr/>
 
@@ -93,4 +113,67 @@
         </div>
     </div>
 </div>
+
+{{-- Single delete modal --}}
+<div class="modal modal-danger fade" tabindex="-1" id="delete_modal" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="{{ __('voyager.generic.close') }}"><span
+                            aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title"><i class="voyager-trash"></i> {{ __('voyager.generic.delete_question') }} {{ strtolower($relationshipDataType->display_name_singular) }}?</h4>
+            </div>
+            <div class="modal-footer">
+                <form action="{{ route('voyager.'.$relationshipDataType->slug.'.index') }}" id="delete_form" method="POST">
+                    {{ method_field("DELETE") }}
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-danger pull-right delete-confirm"
+                             value="{{ __('voyager.generic.delete_confirm') }} {{ strtolower($relationshipDataType->display_name_singular) }}">
+                </form>
+                <button type="button" class="btn btn-default pull-right" data-dismiss="modal">{{ __('voyager.generic.cancel') }}</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 @endpush
+
+@section('javascript')
+    <!-- DataTables -->
+    @if(!$relationshipDataType->server_side && config('dashboard.data_tables.responsive'))
+        <script src="{{ voyager_asset('lib/js/dataTables.responsive.min.js') }}"></script>
+    @endif
+    <script>
+        $(document).ready(function () {
+            @if (!$relationshipDataType->server_side)
+                var table = $('#dataTable').DataTable({!! json_encode(
+                    array_merge([
+                        "order" => [],
+                        "language" => __('voyager.datatable'),
+                    ],
+                    config('voyager.dashboard.data_tables', []))
+                , true) !!});
+            @else
+                $('#search-input select').select2({
+                    minimumResultsForSearch: Infinity
+                });
+            @endif
+        });
+
+
+        var deleteFormAction;
+        $('td').on('click', '.delete', function (e) {
+            var form = $('#delete_form')[0];
+
+            if (!deleteFormAction) { // Save form action initial value
+                deleteFormAction = form.action;
+            }
+
+            form.action = deleteFormAction.match(/\/[0-9]+$/)
+                ? deleteFormAction.replace(/([0-9]+$)/, $(this).data('id'))
+                : deleteFormAction + '/' + $(this).data('id');
+            console.log(form.action);
+
+            $('#delete_modal').modal('show');
+        });
+    </script>
+@stop
