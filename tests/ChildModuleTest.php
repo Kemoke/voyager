68300d2e466434c7937651179b4daefb3d34b1b3
vblinden <me@vblinden.com>
2018-02-26 12:34:12 +0100

    Added a test for child module

--- tests/ChildModuleTest.php
+++ tests/ChildModuleTest.php
@@ -4,8 +4,10 @@ namespace TCG\Voyager\Tests;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Auth;
 use TCG\Voyager\Facades\Voyager;
 use TCG\Voyager\FormFields\AbstractHandler;
+use TCG\Voyager\Models\DataRow;
 
 class ChildModuleTest extends TestCase
 {
@@ -16,6 +18,8 @@ class ChildModuleTest extends TestCase
         parent::setUp();
 
         $this->install();
+
+        Auth::loginUsingId(1);
     }
 
     /** @test */
@@ -26,6 +30,33 @@ class ChildModuleTest extends TestCase
 
         $this->assertTrue(Voyager::formFieldsRelationship()->keys()->contains('dummy'));
         $this->assertFalse(Voyager::formFields()->keys()->contains('dummy'));
+    }
+
+    /** @test */
+    public function user_has_role_child_modile()
+    {
+        $row = DataRow::where('field', 'user_belongsto_role_relationship')->first();
+        $row->details = json_encode([
+            "model" => "TCG\\Voyager\\Models\\Role",
+            "input_type" => "child_module",
+            "table" => "roles",
+            "type" => "hasOne",
+            "column" => "id",
+            "key" => "id",
+            "label" => "name",
+            "pivot_table" => "roles",
+            "pivot" => "0"
+        ]);
+        
+        $row->save();
+
+        $this->visit(route('voyager.users.index'))->click(__('voyager.generic.add_new'))->seePageIs(route('voyager.users.create'));
+        $this->assertResponseStatus(200);
+        $this->see('Add User');
+
+        $this->see('admin');
+        $this->see('user');
+        $this->see('Add a new role');
     }
 }
 
